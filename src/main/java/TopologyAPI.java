d9673952292365d67127f3030008a95a7bda75ee
KarimElsayad247 <karimelsayad1999@gmail.com>
2021-12-20 20:42:07 +0200

    Create scaffolding for API

--- src/main/java/TopologyAPI.java
+++ src/main/java/TopologyAPI.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class TopologyAPI {
+public class TopologyAPI {
+    private Topology[] topologies;
+
+    public void readJSON(String fileName) {
+
+    }
+
+    public void writeJSON(String topologyID) {
+
+    }
+
+    public Topology[] queryTopologies() {
+        return null;
+    }
+
+    public void deleteToppology(String topologyID) {
+
+    }
+
+    public Component[] queryDevices(String topologyID) {
+        return null;
+    }
+
+    public Component[] queryDevicesWithNetlistNode(String topologyID, String netlistNodeID) {
+        return null;
+    }
+
 }
+
