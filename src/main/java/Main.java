f78c42269cff8de4590bd0b31f4c92d522a0f87e
KarimElsayad247 <karimelsayad1999@gmail.com>
2021-12-21 00:21:43 +0200

    Test custom deserializer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, friends!");
+        GsonBuilder gsonBuilder   = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(Topology.class, new TopologyAPI.TopologyDeserilizer());
+        Gson gson = gsonBuilder.create();
+        System.out.println("Current dir is: " + System.getProperty("user.dir"));
+        Path path = Path.of("./target/classes/topology.json");
+        try {
+            String content = Files.readString(path, StandardCharsets.US_ASCII);
+            System.out.println(content);
+            Topology topology = gson.fromJson(content, Topology.class);
+            System.out.println(topology.getId());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Can't open file!");
+            return;
+        }
+
     }
 }
