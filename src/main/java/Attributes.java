0866c1abe2dcdd142b5a23649cda4220f12a5d05
KarimElsayad247 <karimelsayad1999@gmail.com>
2021-12-25 12:54:00 +0200

    Change class to record

--- src/main/java/Attributes.java
+++ src/main/java/Attributes.java
@@ -1,32 +1,4 @@
 /**
- * Simple POJO to store attributes
+ * A record to store component attributes (e.g. resistance)
  */
-public class Attributes {
-    private String name;
-    private double defaultValue;
-    private double minValue;
-    private double maxValue;
-
-    public Attributes(String name, double defaultValue, double minValue, double maxValue) {
-        this.name = name;
-        this.defaultValue = defaultValue;
-        this.minValue = minValue;
-        this.maxValue = maxValue;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getDefaultValue() {
-        return defaultValue;
-    }
-
-    public double getMinValue() {
-        return minValue;
-    }
-
-    public double getMaxValue() {
-        return maxValue;
-    }
-}
+public record Attributes(String name, double defaultValue, double minValue, double maxValue) {}
